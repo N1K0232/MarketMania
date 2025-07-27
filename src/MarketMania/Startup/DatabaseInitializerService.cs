fe0cf72433f90cdf7f8e7e524d650fcc02b0fa5c
N1K0232 <ns.nicolasilvestri@libero.it>
2025-07-27 19:06:28 +0200

    Add startup service for db

---
+++ src/MarketMania/Startup/DatabaseInitializerService.cs
@@ -0,0 +1,20 @@
+﻿
+using MarketMania.DataAccessLayer;
+using Microsoft.EntityFrameworkCore;
+
+namespace MarketMania.Startup;
+
+public class DatabaseInitializerService(IServiceProvider serviceProvider) : IHostedService
+{
+    public async Task StartAsync(CancellationToken cancellationToken)
+    {
+        await using var scope = serviceProvider.CreateAsyncScope();
+        var applicationDbContext = scope.ServiceProvider.GetRequiredService<ApplicationDbContext>();
+
+        await applicationDbContext.Database.EnsureCreatedAsync(cancellationToken);
+        await applicationDbContext.Database.MigrateAsync(cancellationToken);
+    }
+
+    public Task StopAsync(CancellationToken cancellationToken)
+        => Task.CompletedTask;
+}
