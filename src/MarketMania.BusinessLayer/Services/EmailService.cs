81853f77e2f3af6460f0e91c65cadd5f91f59aea
N1K0232 <ns.nicolasilvestri@libero.it>
2025-08-12 21:53:55 +0200

    Add EmailService

---
+++ src/MarketMania.BusinessLayer/Services/EmailService.cs
@@ -0,0 +1,20 @@
+﻿using MarketMania.BusinessLayer.Services.Interfaces;
+using MarketMania.Clients.Interfaces;
+using MarketMania.Clients.Models.Email;
+using OperationResults;
+
+namespace MarketMania.BusinessLayer.Services;
+
+public class EmailService(IEmailClient emailClient) : IEmailService
+{
+    public async Task<Result> SendAsync(EmailMessage emailMessage, CancellationToken cancellationToken)
+    {
+        var response = await emailClient.SendAsync(emailMessage, cancellationToken);
+        if (response.Succeed)
+        {
+            return Result.Ok();
+        }
+
+        return Result.Fail(FailureReasons.ClientError, "Unable to send the email", response.ErrorMessage);
+    }
+}
---
+++ src/MarketMania.BusinessLayer/Services/Interfaces/IEmailService.cs
@@ -0,0 +1,9 @@
+﻿using MarketMania.Clients.Models.Email;
+using OperationResults;
+
+namespace MarketMania.BusinessLayer.Services.Interfaces;
+
+public interface IEmailService
+{
+    Task<Result> SendAsync(EmailMessage emailMessage, CancellationToken cancellationToken);
+}
