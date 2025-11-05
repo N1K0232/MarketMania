5ec89b76c301a5dc43f2f8d7284375bc2c49b8b4
N1K0232 <ns.nicolasilvestri@libero.it>
2025-11-05 21:36:11 +0100

    refactoring

--- src/MarketMania.DataAccessLayer/ApplicationDbContext.cs
+++ src/MarketMania.DataAccessLayer/ApplicationDbContext.cs
@@ -1,27 +1,27 @@
-﻿using System.Data;
-using System.Reflection;
+﻿using System.Reflection;
 using EntityFramework.Exceptions.SqlServer;
 using MarketMania.Authentication;
 using MarketMania.DataAccessLayer.Entities.Common;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
 
 namespace MarketMania.DataAccessLayer;
 
-public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : AuthenticationDbContext(options), IApplicationDbContext
+public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options, ILogger<ApplicationDbContext> logger) : AuthenticationDbContext(options), IApplicationDbContext
 {
-    public Task DeleteAsync<T>(T entity, CancellationToken cancellationToken) where T : BaseEntity
+    public Task DeleteAsync<T>(T entity, CancellationToken cancellationToken = default) where T : BaseEntity
     {
         Set<T>().Remove(entity);
-        return Task.CompletedTask;
+        return SaveAsync(cancellationToken);
     }
 
-    public Task DeleteAsync<T>(IEnumerable<T> entities, CancellationToken cancellationToken) where T : BaseEntity
+    public Task DeleteAsync<T>(IEnumerable<T> entities, CancellationToken cancellationToken = default) where T : BaseEntity
     {
         Set<T>().RemoveRange(entities);
-        return Task.CompletedTask;
+        return SaveAsync(cancellationToken);
     }
 
-    public async ValueTask<T?> GetAsync<T>(Guid id, CancellationToken cancellationToken) where T : BaseEntity
+    public async ValueTask<T?> GetAsync<T>(Guid id, CancellationToken cancellationToken = default) where T : BaseEntity
     {
         var entity = await Set<T>().FindAsync([id], cancellationToken).ConfigureAwait(false);
         return entity;
@@ -30,16 +30,36 @@ public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options
     public IQueryable<T> GetData<T>(bool trackingChanges = false) where T : BaseEntity
     {
         var set = Set<T>();
-        return trackingChanges ? set.AsTracking() : set.AsNoTrackingWithIdentityResolution();
+        return trackingChanges ? set : set.AsNoTrackingWithIdentityResolution();
     }
 
-    public async Task InsertAsync<T>(T entity, CancellationToken cancellationToken) where T : BaseEntity
+    public async Task InsertAsync<T>(T entity, CancellationToken cancellationToken = default) where T : BaseEntity
     {
         ArgumentNullException.ThrowIfNull(entity, nameof(entity));
         await Set<T>().AddAsync(entity, cancellationToken).ConfigureAwait(false);
     }
 
-    public async Task SaveAsync(CancellationToken cancellationToken)
+    public async Task ExecuteTransactionAsync(Func<CancellationToken, Task> action, CancellationToken cancellationToken)
+    {
+        var strategy = Database.CreateExecutionStrategy();
+        logger.LogInformation("Starting transaction execution");
+
+        await strategy.ExecuteAsync(async (token) => await ExecuteTransactionInternalAsync(action, token), cancellationToken).ConfigureAwait(false);
+        logger.LogInformation("Transaction execution completed");
+    }
+
+    public async Task<T> ExecuteTransactionAsync<T>(Func<CancellationToken, Task<T>> action, CancellationToken cancellationToken)
+    {
+        var strategy = Database.CreateExecutionStrategy();
+        logger.LogInformation("Starting transaction execution");
+
+        var result = await strategy.ExecuteAsync(async (token) => await ExecuteTransactionInternalAsync(action, token), cancellationToken).ConfigureAwait(false);
+        logger.LogInformation("Transaction execution completed");
+
+        return result;
+    }
+
+    public async Task<int> SaveAsync(CancellationToken cancellationToken = default)
     {
         var entries = ChangeTracker.Entries()
             .Where(e => typeof(BaseEntity).IsAssignableFrom(e.Entity.GetType()))
@@ -47,27 +67,26 @@ public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options
 
         foreach (var entry in entries.Where(e => e.State is EntityState.Added or EntityState.Modified))
         {
-            var entity = entry.Entity as BaseEntity;
+            var entity = (BaseEntity)entry.Entity;
 
             if (entry.State is EntityState.Modified)
             {
-                entity!.LastModifiedAt = DateTime.UtcNow;
+                entity.LastModifiedAt = DateTime.UtcNow;
             }
         }
 
-        await SaveChangesAsync(true, cancellationToken).ConfigureAwait(false);
-    }
+        try
+        {
+            var result = await SaveChangesAsync(true, cancellationToken).ConfigureAwait(false);
+            logger.LogDebug("Successfully updated {result} rows", result);
 
-    public async Task ExecuteTransactionAsync(Func<CancellationToken, Task> action, CancellationToken cancellationToken)
-    {
-        var strategy = Database.CreateExecutionStrategy();
-        await strategy.ExecuteAsync((token) => ExecuteTransactionInternalAsync(action, token), cancellationToken).ConfigureAwait(false);
-    }
-
-    public async Task<T> ExecuteTransactionAsync<T>(Func<CancellationToken, Task<T>> action, CancellationToken cancellationToken)
-    {
-        var strategy = Database.CreateExecutionStrategy();
-        return await strategy.ExecuteAsync((token) => ExecuteTransactionInternalAsync(action, token), cancellationToken).ConfigureAwait(false);
+            return result;
+        }
+        catch (Exception ex)
+        {
+            logger.LogError(ex, "Operation didn't succeed");
+            throw;
+        }
     }
 
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
@@ -86,23 +105,29 @@ public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options
 
     private async Task ExecuteTransactionInternalAsync(Func<CancellationToken, Task> action, CancellationToken cancellationToken)
     {
-        await using var transaction = await Database.BeginTransactionAsync(cancellationToken).ConfigureAwait(false);
+        var transaction = await Database.BeginTransactionAsync(cancellationToken).ConfigureAwait(false);
 
         try
         {
             await action.Invoke(cancellationToken).ConfigureAwait(false);
             await transaction.CommitAsync(cancellationToken).ConfigureAwait(false);
         }
-        catch
+        catch (Exception ex)
         {
+            logger.LogError(ex, "Error while executing transaction");
             await transaction.RollbackAsync(cancellationToken).ConfigureAwait(false);
+
             throw;
+        }
+        finally
+        {
+            await transaction.DisposeAsync().ConfigureAwait(false);
         }
     }
 
     private async Task<T> ExecuteTransactionInternalAsync<T>(Func<CancellationToken, Task<T>> action, CancellationToken cancellationToken)
     {
-        await using var transaction = await Database.BeginTransactionAsync(cancellationToken).ConfigureAwait(false);
+        var transaction = await Database.BeginTransactionAsync(cancellationToken).ConfigureAwait(false);
 
         try
         {
@@ -111,10 +136,16 @@ public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options
 
             return result;
         }
-        catch
+        catch (Exception ex)
         {
+            logger.LogError(ex, "Error while executing transaction");
             await transaction.RollbackAsync(cancellationToken).ConfigureAwait(false);
+
             throw;
+        }
+        finally
+        {
+            await transaction.DisposeAsync().ConfigureAwait(false);
         }
     }
 }
--- src/MarketMania.DataAccessLayer/IApplicationDbContext.cs
+++ src/MarketMania.DataAccessLayer/IApplicationDbContext.cs
@@ -4,19 +4,19 @@ namespace MarketMania.DataAccessLayer;
 
 public interface IApplicationDbContext
 {
-    Task DeleteAsync<T>(T entity, CancellationToken cancellationToken) where T : BaseEntity;
+    Task DeleteAsync<T>(T entity, CancellationToken cancellationToken = default) where T : BaseEntity;
 
-    Task DeleteAsync<T>(IEnumerable<T> entities, CancellationToken cancellationToken) where T : BaseEntity;
+    Task DeleteAsync<T>(IEnumerable<T> entities, CancellationToken cancellationToken = default) where T : BaseEntity;
 
-    ValueTask<T?> GetAsync<T>(Guid id, CancellationToken cancellationToken) where T : BaseEntity;
+    ValueTask<T?> GetAsync<T>(Guid id, CancellationToken cancellationToken = default) where T : BaseEntity;
 
     IQueryable<T> GetData<T>(bool trackingChanges = false) where T : BaseEntity;
 
-    Task InsertAsync<T>(T entity, CancellationToken cancellationToken) where T : BaseEntity;
+    Task InsertAsync<T>(T entity, CancellationToken cancellationToken = default) where T : BaseEntity;
 
-    Task SaveAsync(CancellationToken cancellationToken);
+    Task<int> SaveAsync(CancellationToken cancellationToken = default);
 
-    Task ExecuteTransactionAsync(Func<CancellationToken, Task> action, CancellationToken cancellationToken);
+    Task ExecuteTransactionAsync(Func<CancellationToken, Task> action, CancellationToken cancellationToken = default);
 
-    Task<T> ExecuteTransactionAsync<T>(Func<CancellationToken, Task<T>> action, CancellationToken cancellationToken);
+    Task<T> ExecuteTransactionAsync<T>(Func<CancellationToken, Task<T>> action, CancellationToken cancellationToken = default);
 }
